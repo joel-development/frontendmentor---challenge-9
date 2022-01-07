d6717b52c28882bad49920fa56c16bd0cc0144e2
Joel Sanchez <joelalejandro.development@gmail.com>
2022-01-07 16:07:49 -0400

    menu mobile completed

---
+++ docs/script/main.js
@@ -0,0 +1,23 @@
+const toggle = document.getElementById('toggle-menu')
+const menu = document.getElementById('menu')
+
+toggle.addEventListener('click', ()=>
+{
+  menu.classList.toggle('menu--on')
+})
+
+menu.addEventListener('click',(e)=>
+{
+  if(e.target.classList.contains('menu__display'))
+  {
+    e.target.classList.toggle('menu__display--active')
+    e.target.nextElementSibling.classList.toggle('sub-menu--active')
+    e.target.children[0].classList.toggle('arrow-icon--active')
+  }
+  else if(e.target.classList.contains('arrow-icon'))
+  {
+    e.target.parentElement.classList.toggle('menu__display--active')
+    e.target.parentElement.nextElementSibling.classList.toggle('sub-menu--active')
+    e.target.classList.toggle('arrow-icon--active')
+  }
+})
