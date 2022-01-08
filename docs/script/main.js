dbcc31405eda04027ff37b6cef19fd058d681215
Joel Sanchez <joelalejandro.development@gmail.com>
2022-01-08 14:41:34 -0400

    section: designed for future, finished

--- docs/script/main.js
+++ docs/script/main.js
@@ -14,7 +14,7 @@ menu.addEventListener('click',(e)=>
     e.target.nextElementSibling.classList.toggle('sub-menu--active')
     e.target.children[0].classList.toggle('arrow-icon--active')
   }
-  else if(e.target.classList.contains('arrow-icon'))
+  else if(e.target.parentElement.classList.contains('menu__display'))
   {
     e.target.parentElement.classList.toggle('menu__display--active')
     e.target.parentElement.nextElementSibling.classList.toggle('sub-menu--active')
