3cb95a862803de1491e91551603a9a49a4f546bf
Joel Sanchez <joelalejandro.development@gmail.com>
2022-01-10 20:12:28 -0400

    accesibility deleted

--- docs/script/main.js
+++ docs/script/main.js
@@ -42,20 +42,3 @@ menu.addEventListener('click',(e)=>
     e.target.classList.toggle('arrow-icon--active')
   }
 })
-
-// for tabindex
-menu.addEventListener('keypress',(e)=>
-{
-  if(e.target.classList.contains('menu__display'))
-  {
-    e.target.classList.toggle('menu__display--active')
-    e.target.nextElementSibling.classList.toggle('sub-menu--active')
-    e.target.children[0].classList.toggle('arrow-icon--active')
-  }
-  else if(e.target.parentElement.classList.contains('menu__display'))
-  {
-    e.target.parentElement.classList.toggle('menu__display--active')
-    e.target.parentElement.nextElementSibling.classList.toggle('sub-menu--active')
-    e.target.classList.toggle('arrow-icon--active')
-  }
-})
